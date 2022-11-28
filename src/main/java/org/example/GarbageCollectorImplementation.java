ee7c02da0ade9912a3d0999dab16420dfb098489
Igor Penkin <ipenkin86@gmail.com>
2022-11-28 11:05:57 +0300

    getBeans

--- src/main/java/org/example/GarbageCollectorImplementation.java
+++ src/main/java/org/example/GarbageCollectorImplementation.java
@@ -1,10 +1,7 @@
 package org.example;
 
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class GarbageCollectorImplementation implements GarbageCollector {
@@ -12,8 +9,16 @@ public class GarbageCollectorImplementation implements GarbageCollector {
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
     Map<String, ApplicationBean> beans = heap.getBeans();
     Deque<StackInfo.Frame> frames = stack.getStack();
-
+    Set<ApplicationBean> beansSet = new HashSet<>(getBeans(beans));
     return new ArrayList<>();
+  }
+
+  private Set<ApplicationBean> getBeans(Map<String,ApplicationBean> beanMap){
+    Set<ApplicationBean> beanSet = new HashSet<>();
+    for (Map.Entry<String,ApplicationBean> beans: beanMap.entrySet()) {
+      beanSet.add(beans.getValue());
+    }
+    return beanSet;
   }
 
 }
