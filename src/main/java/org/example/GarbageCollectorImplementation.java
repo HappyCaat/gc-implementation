ade23ef6c0c43a16ffe8aa988198bba144692b0b
Igor Penkin <ipenkin86@gmail.com>
2022-11-29 14:23:55 +0300

    done

--- src/main/java/org/example/GarbageCollectorImplementation.java
+++ src/main/java/org/example/GarbageCollectorImplementation.java
@@ -10,7 +10,8 @@ public class GarbageCollectorImplementation implements GarbageCollector {
     Map<String, ApplicationBean> beans = heap.getBeans();
     Deque<StackInfo.Frame> frames = stack.getStack();
     Set<ApplicationBean> beansSet = new HashSet<>(getBeans(beans));
-    return new ArrayList<>();
+    Set<ApplicationBean> aliveBeans = getAliveBeans(frames);
+    return getGarbage(aliveBeans, beansSet);
   }
 
   private Set<ApplicationBean> getBeans(Map<String,ApplicationBean> beanMap){
@@ -21,5 +22,28 @@ public class GarbageCollectorImplementation implements GarbageCollector {
     return beanSet;
   }
 
+  private Set <ApplicationBean> getAliveBeans(final Deque<StackInfo.Frame> frames) {
+    Set<ApplicationBean> aliveBeanSet = new HashSet<>();
+    for (StackInfo.Frame frame : frames) {
+      for (ApplicationBean applicationBean : frame.getParameters()) {
+        getChild(applicationBean, aliveBeanSet);
+      }
+    }
+    return aliveBeanSet;
+  }
+
+  private Set<ApplicationBean> getChild(ApplicationBean bean, Set<ApplicationBean> aliveBeansSet) {
+    aliveBeansSet.add(bean);
+    for (ApplicationBean applicationBean : bean.getFieldValues().values()) {
+      if (!aliveBeansSet.contains(applicationBean))
+        aliveBeansSet.addAll(getChild(applicationBean, aliveBeansSet));
+    }
+    return aliveBeansSet;
+  }
+
+  private List<ApplicationBean> getGarbage(Set<ApplicationBean> aliveBeanSet, Set<ApplicationBean> beanSet) {
+    beanSet.removeAll(aliveBeanSet);
+    return new ArrayList<>(beanSet);
+  }
 }
 
